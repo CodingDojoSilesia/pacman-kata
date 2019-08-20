0f533fa16caca750277dda518506d537ebd52dcf
Arkadiusz Nowakowski <a.nowakowski@devesoft.pl>
2019-08-20 08:07:29 +0200

    feat: allow to use javascript and typescript together

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
         "<rootDir>/test"
     ],
     "transform": {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.(js|jsx)?$": "babel-jest"
     }
 }
